80f3c54099829276ff5054a2da6ab54fd201585d
kbility <mohbility@gmail.com>
2018-11-04 21:12:15 -0500

    Refactoring to Multimodule Maven Project. Closes #1

---
+++ pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.mohbility.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.mohbility.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
