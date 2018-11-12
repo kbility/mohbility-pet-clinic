5a4c9de8fd58f8c9c4650886ee59010680185e59
kbility <mohbility@gmail.com>
2018-11-11 21:24:31 -0500

    Updated Visit with JPA entity mappings. Closes #38

--- pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/model/Visit.java
@@ -1,15 +1,25 @@
 package com.mohbility.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * @author home ON ${Date}
  * @project sfg-pet-clinic
  */
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+   @ManyToOne
+   @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
