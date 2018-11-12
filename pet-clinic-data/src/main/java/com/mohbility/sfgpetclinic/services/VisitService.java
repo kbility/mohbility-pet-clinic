a81fd4f9d42db8864c0948c73a44bec550f065a2
kbility <mohbility@gmail.com>
2018-11-12 03:58:28 -0500

    Implemented Mpa impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package com.mohbility.sfgpetclinic.services;
+
+import com.mohbility.sfgpetclinic.model.Visit;
+
+/**
+ * @project sfg-pet-clinic
+ * @author: kbility
+ * @Date: 11/12/2018
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,49 @@
+package com.mohbility.sfgpetclinic.services.map;
+
+import com.mohbility.sfgpetclinic.model.Visit;
+import com.mohbility.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+/**
+ * @project sfg-pet-clinic
+ * @author: kbility
+ * @Date: 11/12/2018
+ */
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.findById(id);
+
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit.getPet()== null || visit.getPet().getOwner()==null || visit.getPet().getId() ==null
+        || visit.getPet().getOwner().getId()==null) {
+            throw new RuntimeException("Invalid visit");
+        }
+
+        return super.save(visit);
+
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
