3bf52c6395f6d0c16226f208deae55d9dfb71437
kbility <mohbility@gmail.com>
2018-11-11 21:03:02 -0500

    Updated owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/model/Person.java
@@ -1,8 +1,15 @@
 package com.mohbility.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name="fist_name")
     private String firstName;
+
+    @Column(name="last_name")
     private String lastName;
 
     public String getFirstName() {
