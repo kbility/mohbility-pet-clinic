db0aaddad50b6cf9e23c12770045cb056de1dd06
kbility <mohbility@gmail.com>
2018-11-11 22:52:49 -0500

    Added spring data jpa service for vets. Closes #41

--- pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/repositories/VetRepository.java
@@ -1,12 +1,12 @@
 package com.mohbility.sfgpetclinic.repositories;
 
 import com.mohbility.sfgpetclinic.model.Vet;
-import com.mohbility.sfgpetclinic.services.CrudService;
+import org.springframework.data.repository.CrudRepository;
 
 /**
  * @project sfg-pet-clinic
  * @author: kbility
  * @Date: 11/11/2018
  */
-public interface VetRepository extends CrudService<Vet,Long> {
+public interface VetRepository extends CrudRepository<Vet,Long> {
 }
---
+++ pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,55 @@
+package com.mohbility.sfgpetclinic.services.springdatajpa;
+
+import com.mohbility.sfgpetclinic.model.Vet;
+import com.mohbility.sfgpetclinic.repositories.VetRepository;
+import com.mohbility.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @project sfg-pet-clinic
+ * @author: kbility
+ * @Date: 11/11/2018
+ */
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets ::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+
+    }
+}
