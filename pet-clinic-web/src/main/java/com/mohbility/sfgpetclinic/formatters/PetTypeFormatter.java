d24b6310dc9eb70035f1279a4de3649e2d6649d9
kbility <mohbility@gmail.com>
2018-12-07 20:08:15 -0500

    Added update and create new Pet Closes #56

---
+++ pet-clinic-web/src/main/java/com/mohbility/sfgpetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,42 @@
+package com.mohbility.sfgpetclinic.formatters;
+
+import com.mohbility.sfgpetclinic.model.PetType;
+import com.mohbility.sfgpetclinic.services.PetTypeService;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+/**
+ * @project sfg-pet-clinic
+ * @author: kbility
+ * @Date: 12/7/2018
+ */
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = this.petTypeService.findAll();
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
